5392b58bd55b8ae3375b18d42d713cfd36966367
ikeda <tam@archway.ddo.jp>
2025-03-28 21:43:58 +0900

    script.pyを進めている

---
+++ MyPython/script.py
@@ -0,0 +1,19 @@
+print("こんにちは！")
+strowberry = 150
+apple_price = 120
+print( type(apple_price))
+print(apple_price)
+print(apple_price + strowberry)
+
+apple_price = "120円"
+print( type(apple_price))
+print(apple_price)
+#print(apple_price + strowberry)
+
+apple_price = 100
+name = "斎藤"
+weight = 54.5
+print ( apple_price, name, weight)
+print ( "apple_price = ", type(apple_price))
+print ( "name = ", type(name))
+print ( "weight = ", type(weight))
